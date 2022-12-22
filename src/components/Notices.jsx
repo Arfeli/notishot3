b95eb60e38fcff8882b818fd443b83c66139d91d
Arfeli <alejandro.arfeli@gmail.com>
2022-12-22 06:59:46 -0300

    una mas

--- src/components/Notices.jsx
+++ src/components/Notices.jsx
@@ -45,6 +45,38 @@ const Notices = () => {
     <div className="main-panel">
       <div className="content-wrapper">
         <div className="row">
+          <div className="col-xs-4 col-sm-6 col-ls-6 col-xl-4 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <div className="item">
+                  <iframe
+                  style={{ width: "250px", height: "250px" }}
+                    
+                    src="https://www.youtube.com/embed/hqKGDK4W_p0"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+                <div className="d-flex py-4">
+                  <div className="preview-list w-100">
+                    <div className="preview-item p-0">
+                      <div className="preview-item-content d-flex flex-grow">
+                        <div className="flex-grow">
+                          <div className="d-flex d-md-block d-xl-flex justify-content-between">
+                            <h6 className="preview-subject"></h6>
+                          </div>
+                          <p className="text-muted"></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <p className="text-muted">Well, it seems to be working now. </p>
+              </div>
+            </div>
+          </div>
           {notas.map((nota) => {
             return (
               <div className="col-xs-4 col-sm-6 col-ls-6 col-xl-4 grid-margin stretch-card">
